76abb0fdc5ba7c10d0cf7885f66a091614260c9d
klb2014 <karterb2020@icloud.com>
2025-08-02 12:23:56 -0400

    gisperg

--- kb robomania/Assets/Scripts/Crusher1.cs
+++ kb robomania/Assets/Scripts/Crusher1.cs
@@ -1,6 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Crusher1
+public class Crusher1 : MonoBehaviour
 {
-    
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+    }
 }
