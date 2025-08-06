92a60e3a923f2d8b428dcd3dbad73d18421c1653
bossnugget15 <karterb2020@icloud.com>
2025-08-06 18:08:13 -0400

    yguj

--- kb robomania/Assets/Scripts/Crusher1.cs
+++ kb robomania/Assets/Scripts/Crusher1.cs
@@ -8,8 +8,13 @@ public class Crusher1 : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (transform.position.x <= -8 || transform.position.x >= 8)
+        {
+            speed *= -1;
+        }
         float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
         float newYPosition = transform.position.y;
         Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
     }
 }
