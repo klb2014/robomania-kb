c4942f0411f2ad9be79c23346d30c6ce3069b9f0
klb2014 <karterb2020@icloud.com>
2025-08-13 17:14:08 -0400

    sdfghjkl;

---
+++ kb robomania/Assets/Scripts/Crusher2.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Crusher2 : MonoBehaviour 
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+ GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+    }
+}
